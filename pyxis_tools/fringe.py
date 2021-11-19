1de00e1cfa4e04f8cbea3663606b7470d6dde783
Adam Rains <adam.d.rains@gmail.com>
2021-11-19 11:37:55 +1100

    Master dark creation function, updated fringe extraction, start of phase extraction.

--- pyxis_tools/fringe.py
+++ pyxis_tools/fringe.py
@@ -1,9 +1,25 @@
 """
+
+
+lambda_1, lambda_2  --> wavelengths for laser 1 and 2
+Lambda_1_2          --> synthetic wavelength from wavelengths 1 and 2
+n_1, n_2            --> fringe orders for wavelengths 1 and 2
+phi_1, phi_2        --> phase for wavelength 1 and 2 respectively
+delta_r_1_2         --> path length difference between laser 1 and 2
+
+
+delta_r_1_2 = Lambda_1_2 * (phi_1 - phi_2) / 2pi        (*)
+
+(*) Under the assumption that delta_n_1_2 is approximately 0, which is true
+where lambda_1 ~= lambda_2.
+
 """
 import numpy as np
 import matplotlib.pyplot as plt
 import pyxis_tools.utils as utils
 import matplotlib.ticker as plticker
+import matplotlib.patches as patches
+from scipy.ndimage import rotate
 from matplotlib.colors import LogNorm, PowerNorm
 
 #------------------------------------------------------------------------------
@@ -11,10 +27,12 @@ from matplotlib.colors import LogNorm, PowerNorm
 #------------------------------------------------------------------------------
 # Camera
 PIXEL_SCALE = 3.45  # um/px
+DETECTOR_SHAPE = (1080, 1440)
 
 # Laser sources
-LAMBDA_1 = 780      # nm
-LAMBDA_2 = 830      # nm
+LAMBDA_1 = 0.780      # um
+LAMBDA_2 = 0.830      # um
+LAMBDA_1_2 = LAMBDA_1 * LAMBDA_2 / (LAMBDA_2 - LAMBDA_1)
 
 # Grating
 GRATING_LINE_SEPARATION = 12.7  # um
@@ -24,7 +42,7 @@ GRATING_BETA_830nm = -55.68     # degrees
 #------------------------------------------------------------------------------
 # Functions
 #------------------------------------------------------------------------------
-def extract_fringe(
+def extract_wavelength_dimension(
     fringe_2d,
     n_fringes_to_extract=6,
     spatial_x0=408,
@@ -33,7 +51,8 @@ def extract_fringe(
     width=4,
     image_plot_x_bounds=(460,590),
     image_plot_y_bounds=(220,500),):
-    """Function to extract a series of fringes from a detector image.
+    """Extracts the _wavelength_ dimension of the camera image - e.g. this will
+    display the discrete modes of the laser diodes.
 
     Currently relies on manually defining a reference point in the spatial and
     wavelength dimensions to start the extraction from, as well as whatever
@@ -118,3 +137,267 @@ def extract_fringe(
     fringes = np.array(fringes)
 
     return fringes
+
+
+def extract_fringes_from_image(
+    fringe_image,
+    dark_image,
+    n_fringes_to_extract=18,
+    spatial_x0=570,
+    spatial_lims=(540,590),
+    lambda_0=638,
+    lambda_lims=(625, 825),
+    lambda_width=6,
+    lambda_sep=10,
+    rotation_angle=0,):
+    """Function to extract a series of fringes from a detector image. 
+
+    Currently relies on manually defining a reference point in the spatial and
+    wavelength dimensions to start the extraction from, as well as whatever
+    angle the fringes have if not incident vertically on the detector.
+
+    Steps:
+     1) Dark correct image,
+     2) Rotate (optional),
+     3) Extract n_fringes_to_extract 1D fringes, binning in the wavelength 
+        dimension,
+     4) Plot a) fringe image with overplotted diagnostics, and b) extracted
+        fringes.
+
+    Parameters
+    ----------
+    fringe_image: float array
+        Camera image, by default with shape (1080, 1440). The short axis is the
+        wavelength dimension, and the long axis is the fringe dimension. This 
+        is transposed upon import if not already to give wavelength on the 
+        horizontal axis and fringes on the vertical axis.
+    
+    dark_image: float array
+        Dark frame of same shape as fringe_image.
+    
+    n_fringes_to_extract: int, default:18
+        The number of fringes to extract.
+
+    spatial_x0: int, default:565
+        Reference plane for spatial (fringe) axis.
+
+    spatial_lims: int tuple, default:(540,590)
+        Limits of the spatial (fringe) axis for plotting.
+
+    lambda_0: int, default:629
+        Reference plane for wavelength axis, corresponding to the start of the
+        first fringe.
+
+    lambda_lims: int tuple, default:(625,825)
+        Limits of the wavelength axis for plotting.
+
+    lambda_width: int, default:6
+        Adopted width of each fringe (in px) in wavelength space.
+
+    lambda_sep: float, default:4.3
+        Adopted separation between subsequent fringes (in px) in wavelength 
+        space.
+
+    rotation_angle: float, default:0
+        Angle in degrees to rotate fringe_image by in the case that the optics
+        are not perfectly aligned. Note that this may introduce artefacts by
+        rotating the pixels, but likely fine for testing.
+
+    Returns
+    -------
+    fringes: float array
+        Array of extracted fringes, of shape (n_fringes_to_extract, n_px_lambda)
+    """
+    # Rotate image by 90 deg if we haven't already. This makes the spatial
+    # dimension our vertical one, and the wavelength dimension horizontal.
+    if fringe_image.shape == DETECTOR_SHAPE:
+        fringe_image = fringe_image.T
+        dark_image = dark_image.T
+    
+    lambdas, spatial_xx = np.meshgrid(
+        np.arange(fringe_image.shape[1]),
+        np.arange(fringe_image.shape[0]),)
+
+    # Initialise output
+    fringes = []
+
+    # Intialise plots
+    plt.close("all")
+    fig, (ax_image, ax_fringe) = plt.subplots(2,1)
+
+    # Dark/bias correct before moving on
+    fringe_image_dc = fringe_image - dark_image
+
+    # Now rotate the image if required
+    if rotation_angle != 0:
+        fringe_image_dc = rotate(fringe_image_dc, angle=rotation_angle, reshape=False)
+
+    img = ax_image.imshow(
+        fringe_image_dc,
+        aspect="equal",
+        origin="lower",
+        norm=PowerNorm(gamma=0.5))
+    cb = fig.colorbar(img, ax=ax_image)
+    cb.set_label("Counts")
+
+    # Hatch in the first section
+    rect = patches.Rectangle(
+            xy=(lambda_lims[0], spatial_lims[0]),
+            width=lambda_0-lambda_lims[0],
+            height=spatial_lims[1]-spatial_lims[0],
+            fill=False,
+            hatch="///",)
+    ax_image.add_patch(rect)
+
+    # And plot our reference line
+    ax_image.hlines(
+        spatial_x0,
+        lambda_lims[0],
+        lambda_lims[1],
+        color="red",
+        linewidth=0.5,)
+
+    colours = plt.cm.plasma(np.linspace(0,1,n_fringes_to_extract))
+
+    # Extract each fringe
+    for fringe_i in range(n_fringes_to_extract):
+        # Calculate the boundaries of the fringe
+        bound_low = lambda_0 + (lambda_width + lambda_sep) * fringe_i
+        bound_high = lambda_0 + (lambda_width + lambda_sep) * fringe_i + lambda_width
+        
+        # Plot the boundaries of the fringe
+        ax_image.vlines(
+            bound_low,
+            spatial_lims[0],
+            spatial_lims[1],
+            color="red",
+            linewidth=0.5,)
+
+        ax_image.vlines(
+            bound_high,
+            spatial_lims[0],
+            spatial_lims[1],
+            color="red",
+            linewidth=0.5,)
+        
+        # Hatch space between
+        if fringe_i == n_fringes_to_extract-1:
+            hatch_width = lambda_lims[1] - bound_high
+        else:
+            hatch_width = lambda_sep
+
+        rect = patches.Rectangle(
+            xy=(bound_high, spatial_lims[0]),
+            width=hatch_width,
+            height=spatial_lims[1]-spatial_lims[0],
+            fill=False,
+            hatch="///",)
+        ax_image.add_patch(rect)
+
+        # Plot fringe number
+        ax_image.text(
+            x=bound_low+lambda_width/2,
+            y=spatial_lims[1]-5,
+            s=fringe_i,
+            color="red",
+            horizontalalignment="center",)
+
+        # Create mask for fringe extraction
+        fringe_start = lambdas > bound_low
+        fringe_end = lambdas < bound_high
+
+        fringe_mask = np.logical_and(fringe_start, fringe_end)
+
+        # Now mask fringe
+        masked_fringe_2d = fringe_image_dc.copy()
+        masked_fringe_2d[~fringe_mask] = 0
+
+        # Collapse into 1D along wavelength dimension (keeping fringe dimension)
+        fringe = np.sum(masked_fringe_2d, axis=1)
+
+        # Plot fringe, subtracting our spatial x0 value
+        scaled_x = (spatial_xx[:,1] - spatial_x0) * PIXEL_SCALE
+
+        ax_fringe.plot(
+            scaled_x,
+            fringe,
+            linewidth=1,
+            color=colours[fringe_i],
+            label="Fringe {:0.0f}".format(fringe_i))
+        
+        fringes.append(fringe)
+
+    # Finalise plots
+    ax_image.set_xlabel(r"$\lambda$ (px)")
+    ax_image.set_ylabel("X (px)")
+    ax_image.set_xlim(lambda_lims)
+    ax_image.set_ylim(spatial_lims)
+    
+    ax_fringe.set_xlim((
+        (spatial_lims[0]-spatial_x0)*PIXEL_SCALE, 
+        (spatial_lims[1]-spatial_x0)*PIXEL_SCALE))
+    ax_fringe.set_xlabel(r"X ($\mu$m)")
+    ax_fringe.set_ylabel(r"Y (counts)")
+    ax_fringe.legend(loc="center right", ncol=2)
+
+    ax_fringe.xaxis.set_major_locator(plticker.MultipleLocator(base=20))
+    ax_fringe.xaxis.set_minor_locator(plticker.MultipleLocator(base=10))
+
+    fringes = np.array(fringes)
+
+    return fringes
+
+
+def extract_fringe_phase(fringes, skip_missing_fringes=False, skip_threshold=10):
+    """Fourier transform each fringe to extract fringe phase. Plots fringe
+    magnitude and phase.
+
+    Parameters
+    ----------
+    fringes: float array
+        Array of extracted fringes, of shape (n_fringes_to_extract, n_px_lambda)
+
+    skip_missing_fringes: boolean, default:False
+        Whether to skip fringes that have no signal.
+
+    skip_threshold: int, default:10
+        Threshold signal level associated with skip_missing_fringes.
+    """
+    # Initialise plot
+    plt.close("all")
+    fig, (ax_mag, ax_phase) = plt.subplots(1,2)
+
+    # Initialise colours
+    colours = plt.cm.plasma(np.linspace(0,1,len(fringes)))
+
+    # Plot magnitude and phase for every fringe
+    for fringe_i in range(len(fringes)):
+        # Only plot if our fringe actually has data
+        if skip_missing_fringes and np.sum(fringes[fringe_i]) < skip_threshold:
+            continue
+        
+        # Do Fourier Transform and extract fringe magnitude and phase
+        out = np.fft.fft(fringes[fringe_i])
+        mag = np.sqrt(out.real**2 + out.imag**2)
+        phase = np.arctan(out.imag / out.real)
+        
+        # Plot magnitude and phase
+        ax_mag.plot(
+            np.fft.fftshift(mag),
+            label="Fringe {}".format(fringe_i),
+            linewidth=0.5,
+            color=colours[fringe_i],)
+        ax_phase.plot(
+            np.fft.fftshift(phase),
+            label="Fringe {}".format(fringe_i),
+            linewidth=0.5,
+            color=colours[fringe_i],)
+    
+    # Finish setting up plots
+    fig.suptitle("FFT of Fringes")
+    ax_mag.set_title("Magnitude")
+    ax_mag.set_ylabel("|counts|")
+    ax_mag.legend(loc="center right")
+    ax_phase.set_title("Phase")
+    ax_phase.set_ylabel("Phase (rad)")
+    ax_phase.legend(loc="center right")
--- pyxis_tools/utils.py
+++ pyxis_tools/utils.py
@@ -1,20 +1,50 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-def read_raw8(filename, shape=(1080,1440)):
-    """ Read in the raw data and return as a numpy array. 
-    
-    It seems that the data was saved in 8 bit format - this has to be changed
-    to dtype=np.uint16 if there is a 16 bit data option.
-    """
-    raw = open(filename, 'rb')
-    im = np.frombuffer(raw.read(), dtype=np.uint8).reshape(shape)
-    return im
-
-#Some test code to show the data.
-if __name__=="__main__":
-    im_uint8 = read_raw8('785_fringe_1.raw')
-    im_float = im_uint8.astype(np.float)
-    #Show an image with square root scaling...
-    plt.clf()
+import os
+import glob
+import numpy as np
+import matplotlib.pyplot as plt
+
+def read_raw8(filename, shape=(1080,1440)):
+
+    """ Read in the raw data and return as a numpy array. 
+
+    
+
+    It seems that the data was saved in 8 bit format - this has to be changed
+
+    to dtype=np.uint16 if there is a 16 bit data option.
+
+    """
+
+    raw = open(filename, 'rb')
+
+    im = np.frombuffer(raw.read(), dtype=np.uint8).reshape(shape)
+
+    return im
+
+
+def create_master_dark(path, dark_fn_with_wildcard):
+    """Create master dark file from median of N darks.
+    """
+    filepath_base = os.path.join(path, dark_fn_with_wildcard)
+    dark_filepaths = glob.glob(filepath_base)
+
+    darks = np.array([read_raw8(fp).astype(float) for fp in dark_filepaths])
+
+    median_dark = np.median(darks, axis=0)
+
+    return median_dark
+
+
+#Some test code to show the data.
+
+if __name__=="__main__":
+
+    im_uint8 = read_raw8('785_fringe_1.raw')
+
+    im_float = im_uint8.astype(np.float)
+
+    #Show an image with square root scaling...
+
+    plt.clf()
+
     plt.imshow(im_float[200:500,400:600]**0.5, aspect='auto')
